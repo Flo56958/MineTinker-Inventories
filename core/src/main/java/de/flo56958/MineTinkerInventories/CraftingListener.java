6e055c855640c4bddfd97cba1b1dfa2c9204f05a
Flo56958 <florian.gutsche@fau.de>
2019-10-11 00:43:56 +0200

    First Implementation (does not work correctly)

--- core/src/main/java/de/flo56958/MineTinkerInventories/CraftingListener.java
+++ core/src/main/java/de/flo56958/MineTinkerInventories/CraftingListener.java
@@ -1,6 +1,18 @@
 package de.flo56958.MineTinkerInventories;
 
+import de.flo56958.MineTinker.Utilities.nms.NBTUtils;
+import org.bukkit.Material;
+import org.bukkit.block.ShulkerBox;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.CraftingInventory;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CraftingListener implements Listener {
 
@@ -20,4 +32,73 @@ public class CraftingListener implements Listener {
 	 */
 
 	//normal shulkers have dimension 0 or no nbt tag
+
+	@EventHandler(ignoreCancelled = true)
+	public void onCraftPrepare(PrepareItemCraftEvent e) {
+		CraftingInventory inv = e.getInventory();
+		ItemStack[] matrix = inv.getMatrix();
+
+		if (matrix[1] != null && matrix[1].getType() == Material.NETHER_STAR) {
+			if (matrix[7] != null && matrix[7].getType() == Material.NETHER_STAR) {
+				if (matrix[0] != null && matrix[0].getType() == Material.SHULKER_BOX) {
+					final int dimension = NBTUtils.getHandler().getInt(matrix[0], "MT-InventoryDimension");
+					if (matrix[2] != null && matrix[2].getType() == Material.SHULKER_BOX && NBTUtils.getHandler().getInt(matrix[2], "MT-InventoryDimension") == dimension) {
+						if (matrix[3] != null && matrix[3].getType() == Material.SHULKER_BOX && NBTUtils.getHandler().getInt(matrix[3], "MT-InventoryDimension") == dimension) {
+							if (matrix[4] != null && matrix[4].getType() == Material.SHULKER_BOX && NBTUtils.getHandler().getInt(matrix[4], "MT-InventoryDimension") == dimension) {
+								if (matrix[5] != null && matrix[5].getType() == Material.SHULKER_BOX && NBTUtils.getHandler().getInt(matrix[5], "MT-InventoryDimension") == dimension) {
+									if (matrix[6] != null && matrix[6].getType() == Material.SHULKER_BOX && NBTUtils.getHandler().getInt(matrix[6], "MT-InventoryDimension") == dimension) {
+										if (matrix[8] != null && matrix[8].getType() == Material.SHULKER_BOX && NBTUtils.getHandler().getInt(matrix[8], "MT-InventoryDimension") == dimension) {
+											ItemStack item = createShulker(dimension + 1);
+											if (item.getItemMeta() instanceof BlockStateMeta) {
+												BlockStateMeta im = (BlockStateMeta) item.getItemMeta();
+												if (im.getBlockState() instanceof ShulkerBox) {
+													ShulkerBox shulker = (ShulkerBox) im.getBlockState();
+													Inventory s_inv = shulker.getInventory(); //FIXME: Inventory is instantly removed again as a inventory
+													for (int i = 0; i < 9; i++) {
+														if (i == 1 || i == 7) continue;
+														s_inv.setItem(i, matrix[i]);
+													}
+
+
+													inv.setResult(item);
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private ItemStack createShulker(int dimension) {
+		ItemStack item = new ItemStack(Material.SHULKER_BOX);
+		if (dimension == 0) return item;
+		if (item.getItemMeta() instanceof BlockStateMeta) {
+			BlockStateMeta im = (BlockStateMeta) item.getItemMeta();
+			if (im.getBlockState() instanceof ShulkerBox) {
+				ShulkerBox shulker = (ShulkerBox) im.getBlockState();
+				List<String> lore = im.getLore();
+				if (lore == null) {
+					lore = new ArrayList<>();
+				}
+				lore.add("Dimension: " + dimension);
+				im.setLore(lore);
+				item.setItemMeta(im);
+
+				NBTUtils.getHandler().setInt(item, "MT-InventoryDimension", dimension);
+
+				Inventory s_inv = shulker.getInventory();
+				ItemStack filler = createShulker(dimension - 1);
+				for (int i = 0; i < s_inv.getSize(); i++) {
+					s_inv.setItem(i, filler.clone());
+				}
+			}
+		}
+
+		return item;
+	}
 }
--- core/src/main/java/de/flo56958/MineTinkerInventories/ShulkerboxListener.java
+++ core/src/main/java/de/flo56958/MineTinkerInventories/ShulkerboxListener.java
@@ -1,9 +1,11 @@
 package de.flo56958.MineTinkerInventories;
 
 import de.flo56958.MineTinker.Utilities.nms.NBTUtils;
+import de.flo56958.MineTinker.api.gui.ButtonAction;
 import de.flo56958.MineTinker.api.gui.GUI;
 import org.bukkit.Material;
 import org.bukkit.block.ShulkerBox;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -36,6 +38,8 @@ public class ShulkerboxListener implements Listener {
 				if (item.getItemMeta() instanceof BlockStateMeta) {
 					BlockStateMeta im = (BlockStateMeta) item.getItemMeta();
 					if (im.getBlockState() instanceof ShulkerBox) {
+						e.setCancelled(true);
+
 						ShulkerBox shulker = (ShulkerBox) im.getBlockState();
 						int dimension = NBTUtils.getHandler().getInt(item, "MT-InventoryDimension");
 						if (dimension <= 0) {
@@ -43,20 +47,61 @@ public class ShulkerboxListener implements Listener {
 							return;
 						}
 
+						GUI.Window window = null;
+
 						for (GUI g : Main.guis) {
-							//TODO: search if a gui was created for shulker dimension
+							window = g.getWindowFromInventory(shulker.getInventory());
+							if(window != null) break;
 						}
 
-						//TODO: create if not found
-
-						//this event will not trigger inside a gui
-
-						//make buttonaction with runnable that will open the inventory of the clicked shulker (left click)
+						if (window == null) {
+							GUI ret = createShulkerGUI(shulker, dimension);
+							if (e.getWhoClicked() instanceof Player) {
+								ret.show((Player) e.getWhoClicked());
+							}
+						}
 
 						//TODO: Change GUI constructor so it can register Events from different plugins
 					}
 				}
 			}
 		}
+	}
+
+	private GUI createShulkerGUI(ShulkerBox shulker, int dimension) {
+		GUI gui = new GUI();
+		Main.guis.add(gui);
+		GUI.Window window = gui.addWindow(shulker.getInventory());
+
+		for (int i = 0; i < shulker.getInventory().getSize(); i++) {
+			ItemStack slot = shulker.getInventory().getItem(i);
+			if (slot == null) continue;
+			if (slot.getItemMeta() instanceof BlockStateMeta) {
+				BlockStateMeta im = (BlockStateMeta) slot.getItemMeta();
+				if (im.getBlockState() instanceof ShulkerBox) {
+					GUI.Window.Button button = window.addButton(i, slot);
+					if (dimension == 1) {
+						button.addAction(ClickType.LEFT, new ButtonAction.RUN_RUNNABLE_ON_PLAYER(button, ((player, s) -> player.openInventory(shulker.getInventory()))));
+					} else {
+						GUI.Window window2 = null;
+
+						for (GUI g : Main.guis) {
+							window2 = g.getWindowFromInventory(shulker.getInventory());
+							if(window2 != null) break;
+						}
+
+						if (window2 == null) {
+							GUI ret = createShulkerGUI((ShulkerBox) slot, dimension - 1);
+							button.addAction(ClickType.LEFT, new ButtonAction.PAGE_GOTO(button, ret.getWindow(0)));
+						} else {
+							button.addAction(ClickType.LEFT, new ButtonAction.PAGE_GOTO(button, window2));
+						}
+
+					}
+				}
+			}
+		}
+
+		return gui;
 	}
 }
